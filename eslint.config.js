a793ce6bd7511b72757563fa58b9ae2623fb4c92
tahatms12 <tahatms@gmail.com>
2025-06-11 08:33:49 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
+diff --git a//dev/null b/uplifttech/eslint.config.js
+index 0000000000000000000000000000000000000000..82c2e20ccc2bae01b6589f5f391cb20f34db41fd 100644
+--- a//dev/null
++++ b/uplifttech/eslint.config.js
+@@ -0,0 +1,28 @@
++import js from '@eslint/js';
++import globals from 'globals';
++import reactHooks from 'eslint-plugin-react-hooks';
++import reactRefresh from 'eslint-plugin-react-refresh';
++import tseslint from 'typescript-eslint';
++
++export default tseslint.config(
++  { ignores: ['dist'] },
++  {
++    extends: [js.configs.recommended, ...tseslint.configs.recommended],
++    files: ['**/*.{ts,tsx}'],
++    languageOptions: {
++      ecmaVersion: 2020,
++      globals: globals.browser,
++    },
++    plugins: {
++      'react-hooks': reactHooks,
++      'react-refresh': reactRefresh,
++    },
++    rules: {
++      ...reactHooks.configs.recommended.rules,
++      'react-refresh/only-export-components': [
++        'warn',
++        { allowConstantExport: true },
++      ],
++    },
++  }
++);
